22009058ff775499ff45c797cbdf2636c220b61d
Josh Schairbaum <joshua.schairbaum@gmail.com>
2021-08-22 22:10:33 -0500

    Added dynamic bits for time in CMH and the current date

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>How many days since Michigan has beaten Ohio State?</h1>
+      <p id="days_since"><span id="days_count">{Math.floor((new Date().getTime() - new Date("11/26/2011").getTime()) / (1000 * 3600 * 24))}</span> days.</p>
+      <p id="phrase">It's <span id="time">{new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York',
+                                                                                    hour: '2-digit',
+                                                                                    minute: '2-digit'
+                                                                                  })}</span> in Columbus, and Michigan still sucks.</p>
+      <footer className="App-footer">
+        an <a href="https://twitter.com/jschairb"
+              target="_blank"
+              rel="noopener noreferrer">@<span class="danzig">jschairb</span></a> joint
+      </footer>
     </div>
   );
 }
