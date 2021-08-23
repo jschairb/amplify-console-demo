18058dd9fcc929d17eb1146da4d43e46f79e80ba
Josh Schairbaum <joshua.schairbaum@gmail.com>
2021-08-23 18:57:57 -0500

    modifying footer to be a component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import logo from './logo.svg';
+import Footer from './Footer.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>How many days since Michigan has beaten Ohio State?</h1>
       <p id="days_since"><span id="days_count">{Math.floor((new Date().getTime() - new Date("11/26/2011").getTime()) / (1000 * 3600 * 24))}</span> days.</p>
       <p id="phrase">It's <span id="time">{new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York',
                                                                                     hour: '2-digit',
                                                                                     minute: '2-digit'
                                                                                   })}</span> in Columbus, and Michigan still sucks.</p>
-      <footer className="App-footer">
-        an <a href="https://twitter.com/jschairb"
-              target="_blank"
-              rel="noopener noreferrer">@<span class="danzig">jschairb</span></a> joint
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="App-footer">
+        an <a href="https://twitter.com/jschairb"
+              target="_blank"
+              rel="noopener noreferrer">@<span class="danzig">jschairb</span></a> joint
+      </footer>
+    )
+  }
+}
+
+export default Footer
